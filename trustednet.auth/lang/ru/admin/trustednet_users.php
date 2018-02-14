45099e63eb4947528d10e670714678f46ed20d22
vva <vva@digt.ru>
2018-02-14 10:49:55 +0300

    Iss #15 Table columns renamed

--- trustednet.auth/lang/ru/admin/trustednet_users.php
+++ trustednet.auth/lang/ru/admin/trustednet_users.php
@@ -2,15 +2,15 @@
 $MESS ['TRUSTEDNET_USERS_PAGE_TITLE'] = "Редактирование связей между пользователями Битрикс и TrustedNet";
 
 // Table columns
-$MESS ['TRUSTEDNET_USERS_COL_ID'] = "ID Bitrix";
-$MESS ['TRUSTEDNET_USERS_COL_LOGIN'] = "Логин Bitrix";
-$MESS ['TRUSTEDNET_USERS_COL_NAME'] = "Имя Bitrix";
-$MESS ['TRUSTEDNET_USERS_COL_LAST_NAME'] = "Фамилия Bitrix";
-$MESS ['TRUSTEDNET_USERS_COL_EMAIL'] = "E-Mail Bitrix";
-$MESS ['TRUSTEDNET_USERS_COL_TN_ID'] = "ID TrustedNet";
-$MESS ['TRUSTEDNET_USERS_COL_TN_GIV_NAME'] = "Имя TrustedNet";
-$MESS ['TRUSTEDNET_USERS_COL_TN_FAM_NAME'] = "Фамилия TrustedNet";
-$MESS ['TRUSTEDNET_USERS_COL_TN_EMAIL'] = "E-mail TrustedNet";
+$MESS ['TRUSTEDNET_USERS_COL_ID'] = "ID в Битрикс";
+$MESS ['TRUSTEDNET_USERS_COL_LOGIN'] = "Логин в Битрикс";
+$MESS ['TRUSTEDNET_USERS_COL_NAME'] = "Имя в Битрикс";
+$MESS ['TRUSTEDNET_USERS_COL_LAST_NAME'] = "Фамилия в Битрикс";
+$MESS ['TRUSTEDNET_USERS_COL_EMAIL'] = "E-Mail в Битрикс";
+$MESS ['TRUSTEDNET_USERS_COL_TN_ID'] = "ID в TrustedNet";
+$MESS ['TRUSTEDNET_USERS_COL_TN_GIV_NAME'] = "Имя в TrustedNet";
+$MESS ['TRUSTEDNET_USERS_COL_TN_FAM_NAME'] = "Фамилия в TrustedNet";
+$MESS ['TRUSTEDNET_USERS_COL_TN_EMAIL'] = "E-mail в TrustedNet";
 
 // Pagination text
 $MESS ['TRUSTEDNET_USERS_NAV_TEXT'] = "Пользователи";
