ce8addcfdb1314571d61d19d405346da2dfdb7fa
vva <vva@digt.ru>
2018-02-21 11:45:45 +0300

    Version 1.3.1

--- trustednet.auth/install/version.php
+++ trustednet.auth/install/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.3.0",
-    "VERSION_DATE" => "2018-02-19 9:00:00"
+    "VERSION" => "1.3.1",
+    "VERSION_DATE" => "2018-02-21 9:00:00"
 );
 
