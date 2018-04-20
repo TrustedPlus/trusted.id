944de1689f065ecb59cc1ab2b4eda5e8ff58615e
vva <vva@digt.ru>
2018-04-20 09:48:00 +0300

    Version 1.3.2
    - User relation page optimized for large datasets

--- trustednet.auth/install/version.php
+++ trustednet.auth/install/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.3.1",
-    "VERSION_DATE" => "2018-02-21 9:00:00"
+    "VERSION" => "1.3.2",
+    "VERSION_DATE" => "2018-04-20 9:00:00"
 );
 
