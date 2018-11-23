dc36ab4b56037a9f436f4945e6496305fdac6d13
vva <vva@digt.ru>
2018-11-23 14:19:15 +0300

    Version 1.3.6

--- trusted.id/install/version.php
+++ trusted.id/install/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $arModuleVersion = array(
-    'VERSION' => '1.3.5',
-    'VERSION_DATE' => '2018-09-27 9:00:00'
+    'VERSION' => '1.3.6',
+    'VERSION_DATE' => '2018-11-23 9:00:00'
 );
 
