dc60dd344102fcb88268f1457b3076a9217c989b
Aleksandr Gavrilov <alg@digt.ru>
2023-03-07 10:25:26 +0300

    bump version to 1.5.1

--- trusted.id/install/version.php
+++ trusted.id/install/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $arModuleVersion = array(
-    'VERSION' => '1.5.0',
-    'VERSION_DATE' => '2023-02-21 12:00:00'
+    'VERSION' => '1.5.1',
+    'VERSION_DATE' => '2023-03-08 12:00:00'
 );
 
